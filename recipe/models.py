b27846dcf2b68544c4b7856e82f7aa1311dad9da
Johannes Hattula <johannes.hattula@pulseon.fi>
2025-01-21 18:21:03 +0200

    minor fixes

--- recipe/models.py
+++ recipe/models.py
@@ -5,6 +5,7 @@ from django.contrib.postgres.fields import ArrayField
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
     ingredients = ArrayField()
+    detials = models.CharField()
     photo = models.ImageField(upload_to='photos/', blank=True, null=True)
     instructions = models.CharField()
     #TODO
--- recipe/views.py
+++ recipe/views.py
@@ -7,8 +7,17 @@ from rest_framework.response import Response
 
 #TODO
 @api_view(['POST'])
-def meal_type(request):
+def cuisine_type(request):
+
+    #have the options for the cuisine types 
     return Response()
 
 def meal_selection(request, meal_type):
+
+    #get inventory and filer recipies based on cusine type and what is available in inventory
+
+    #get list of the top 10 recipies, including name and image for display and ID  
     return Response()
+
+def meal_recipe(request):
+    return
