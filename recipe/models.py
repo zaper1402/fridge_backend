f51a17a2a31df3feacb787c288f96ac9342061a0
Johannes Hattula <johannes.hattula@pulseon.fi>
2025-01-21 17:24:58 +0200

    some progress on recipe

--- recipe/models.py
+++ recipe/models.py
@@ -3,4 +3,8 @@ from .enums import CuisineType, MealType
 from django.contrib.postgres.fields import ArrayField
 
 class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+    ingredients = ArrayField()
+    photo = models.ImageField(upload_to='photos/', blank=True, null=True)
+    instructions = models.CharField()
     #TODO
--- recipe/urls.py
+++ recipe/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path() #TODO
+    path('meal_type', views.meal_type, name='meal_type'),
+    path('meal_selection', views.meal_selection, name='meal_selection'), #TODO
 ]
